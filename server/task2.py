15ac94c990c00d14cf0e9d47e6f02596e3b184f1
valentin g <33gva@mail.ru>
2019-06-10 23:59:45 +0300

    исправлено на работе работает, а дома - нет

--- server/task2.py
+++ server/task2.py
@@ -1,26 +1,34 @@
 from tornado.ioloop import IOLoop
 import tornado.httpserver
 import aiohttp
-import requests
 import tornado.web
 import tornado.ioloop
 import asyncio
-
+import requests
 
 async def req():
     async with aiohttp.ClientSession() as session:
         async with session.get('https://services.drova.io/') as resp:
             return resp.status
 
+def req2():
+    resp = requests.get('https://services.drova.io/session-manager/version')
+    return resp
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        result = asyncio.run(req())
-        self.write(str(result))
+    async def get(self):
+        # first version
+        # result = await req()
+        # self.write(str(result))
+
+        # second version
+        loop = asyncio.get_event_loop()
+        result = await loop.run_in_executor(None, req2)
+        self.write(result.content)
 
 def make_app():
     return tornado.web.Application([
-        (r"/", MainHandler),
+    (r"/", MainHandler),
     ])
 
 if __name__ == "__main__":
